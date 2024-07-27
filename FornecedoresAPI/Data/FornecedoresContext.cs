4e33de8bae460cea6b3867600645b0fa57ef5e0a
Guilherme Cabral <gbral.js@gmail.com>
2024-07-26 23:48:50 -0300

    Incluindo esqueleto da api.

---
+++ FornecedoresAPI/Data/FornecedoresContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FornecedoresAPI.Models;
+
+namespace FornecedoresAPI.Data
+{
+    public class FornecedoresContext : DbContext
+    {
+        public FornecedoresContext(DbContextOptions<FornecedoresContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
---
+++ FornecedoresAPI/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace FornecedoresAPI.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Empresa { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+    }
+}
